b53dcfd915d877e233c2476f6e27c2b0b1812b81
fbmnds7 <fbmnds7@fn.de>
2025-05-27 19:48:41 +0200

    Adjust system clock HSE for 100MHz

--- system_clock.c
+++ system_clock.c
@@ -9,28 +9,21 @@
 
 
 void system_clock_init() {
-    /* Enable HSE */
     RCC->CR |= RCC_CR_HSEON;
     while (!(RCC->CR & RCC_CR_HSERDY));
 
-    /* Configure PLL: 25MHz * 336 / 25 / 4 = 84MHz */
-    RCC->PLLCFGR = (25 << RCC_PLLCFGR_PLLM_Pos) |
-                   (336 << RCC_PLLCFGR_PLLN_Pos) |
-                   (0 << RCC_PLLCFGR_PLLP_Pos) | // PLLP = 4
-                   (7 << RCC_PLLCFGR_PLLQ_Pos) | // PLLQ = 7 (48MHz USB)
-                   RCC_PLLCFGR_PLLSRC_HSE;
+    RCC->APB1ENR |= RCC_APB1ENR_PWREN;
+    PWR->CR |= PWR_CR_VOS;
+    RCC->CFGR |= RCC_CFGR_HPRE_DIV1 | RCC_CFGR_PPRE2_DIV2 | RCC_CFGR_PPRE1_DIV4;
+    RCC->PLLCFGR = (8 << RCC_PLLCFGR_PLLN_Pos) | (336 << RCC_PLLCFGR_PLLM_Pos) |
+                   RCC_PLLCFGR_PLLSRC_HSE | (7 << RCC_PLLCFGR_PLLP_Pos);
+
     RCC->CR |= RCC_CR_PLLON;
     while (!(RCC->CR & RCC_CR_PLLRDY));
 
-    /* Set flash latency (2WS for 84MHz at 3.3V) */
-    FLASH->ACR = FLASH_ACR_LATENCY_2WS;
-
-    /* Select PLL as system clock */
-    RCC->CFGR = (RCC->CFGR & ~RCC_CFGR_SW) | RCC_CFGR_SW_PLL;
+    RCC->CFGR |= RCC_CFGR_SW_PLL;
     while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL);
 
-    /* Configure APB1/APB2 prescalers */
-    RCC->CFGR |= RCC_CFGR_PPRE1_DIV2 | // APB1 = 42MHz
-                 RCC_CFGR_PPRE2_DIV1;  // APB2 = 84MHz
-
+    SystemCoreClock = 100000000; // 100 MHz
 }
+
